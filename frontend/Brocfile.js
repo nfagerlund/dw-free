29c6a588ff4737bc30fccd4709432796be1ba47f
Nick Fagerlund <nick.fagerlund@gmail.com>
2020-07-06 22:28:36 -0700

    Add new build-static system in ./frontend

---
+++ frontend/Brocfile.js
@@ -0,0 +1,84 @@
+const { WatchedDir } = require('broccoli-source');
+const Funnel = require('broccoli-funnel');
+const merge = require('broccoli-merge-trees');
+const uglify = require('broccoli-uglify-sourcemap');
+const CleanCSS = require('broccoli-clean-css');
+const Fiber = require('fibers');
+
+import CompileScssMulti from './compile-scss-multi';
+import { execSync } from 'child_process';
+
+export default () => {
+  // TODO: This is tied to LJ::get_all_directories. If that gets simplified, so can this.
+  let myDirs = execSync("find $LJHOME -type d -path *htdocs")
+    .toString().split("\n").filter(s => s !== '');
+  let htdocs = merge(myDirs.map( dir => new WatchedDir(dir) ), {overwrite: true});
+  // DW_DEV must be set to "1" on dev servers.
+  let isProduction = parseInt(process.env.DW_DEV) !== 1;
+
+  // Images: whatever
+  let imgDir = new Funnel(htdocs, {
+    srcDir: 'img',
+    destDir: 'img',
+    annotation: 'Image dir (copy)',
+  });
+
+  // Vanilla ES5 JS: send it through the ES5 uglifier, if in prod.
+  let jsDir = new Funnel(htdocs, {
+    srcDir: 'js',
+    destDir: 'js',
+    annotation: 'JS dir (copy)',
+  });
+
+  if (isProduction) {
+    jsDir = uglify(jsDir, {
+      annotation: 'JS dir (uglify)',
+      hiddenSourceMap: true, // until we stop w/ concat_res, they're useless.
+    });
+  }
+
+  // Vanilla CSS PLUS some maybe weird stuff? just set it down over there, don't jostle it too hard.
+  let stcDir = new Funnel(htdocs, {
+    srcDir: 'stc',
+    destDir: 'stc',
+    annotation: 'stc dir (copy)',
+  });
+
+  // CSS compression for prod:
+  if (isProduction) {
+    let compressed = new CleanCSS(stcDir, {
+      annotation: 'cleaned CSS from stc dir',
+    });
+    stcDir = merge([stcDir, compressed], {
+      annotation: 'stc dir (with cleaned CSS)',
+      overwrite: true,
+    });
+  }
+
+  // SCSS: start w/ an isolated working directory, so the include paths work out
+  // more easily. Then compile to CSS, then move things to the expected final
+  // location.
+  let scssDir = new Funnel(htdocs, {
+    srcDir: 'scss',
+    destDir: '.',
+    annotation: 'SCSS dir (copy to root)',
+  });
+  let sassOptions = {
+    fiber: Fiber, // slightly faster.
+    annotation: 'SCSS dir (convert all)',
+  };
+  if (process.env.NODE_ENV === 'production') {
+    sassOptions.outputStyle = 'compressed';
+  }
+  let scssOutput = new CompileScssMulti([scssDir], sassOptions);
+  let scssFinal = new Funnel(scssOutput, {
+    srcDir: '.',
+    destDir: 'stc/css',
+    annotation: 'SCSS dir (copy to stc/css)',
+  });
+
+  return merge([imgDir, jsDir, stcDir, scssFinal], {
+    annotation: 'Final merge',
+    overwrite: true,
+  });
+}
---
+++ frontend/compile-scss-multi.js
@@ -0,0 +1,82 @@
+// This copies a bunch of code from broccoli-sass-source-maps -- seems that most
+// people want to compile ONE sass file for their app, and that's absolutely not
+// how we roll over here.
+
+// Anyway, here's my thinking: give it an scss directory, and optionally some
+// extra include paths, and have it compile all non-partials to thing.css files. Ask
+// Sass itself what the dependencies for each file were, and use those for
+// MultiFilter's caching/recompiling behavior.
+// Gonna be lazy and expect only a single inputPath, ignoring others.
+
+const MultiFilter = require('broccoli-multifilter');
+const sass = require('sass');
+const walkSync = require('walk-sync');
+const RSVP = require('rsvp');
+const path = require('path');
+const mkdirp = require('mkdirp');
+const fs = require('fs');
+const writeFile = RSVP.denodeify(fs.writeFile);
+
+
+class CompileScssMulti extends MultiFilter {
+    constructor(inputNodes, options) {
+        super(inputNodes, options);
+        options = options || {};
+
+        this.extraIncludePaths = options.includePaths || [];
+
+        this.renderSass = RSVP.denodeify(sass.render);
+        this.sassOptions = {
+            importer: options.importer,
+            functions: options.functions,
+            indentedSyntax: options.indentedSyntax,
+            omitSourceMapUrl: options.omitSourceMapUrl,
+            outputStyle: options.outputStyle,
+            precision: options.precision,
+            sourceComments: options.sourceComments,
+            fiber: options.fiber
+        };
+
+    }
+
+    async build() {
+        // Ignoring more than one inputPath.
+        let inputPath = this.inputPaths[0];
+        // Exclude _partials.scss
+        let inputFiles = walkSync(inputPath).filter( inFile => path.extname(inFile) === '.scss' && path.basename(inFile).slice(0,1) !== '_' );
+
+        return this.buildAndCache(
+            inputFiles,
+            async (relativePath, outputDirectory) => {
+                let fullInputPath = path.join(inputPath, relativePath);
+                let fullOutputPath = path.join(outputDirectory, relativePath.replace(/\.scss$/, '.css'));
+
+                // Make sure there's somewhere to put it
+                mkdirp.sync(path.dirname(fullOutputPath));
+
+                let sassOptions = {
+                    file: fullInputPath,
+                    outFile: fullOutputPath,
+                    includePaths: [
+                        ...this.inputPaths,
+                        ...this.extraIncludePaths
+                    ],
+                };
+                Object.assign(sassOptions, this.sassOptions);
+                let result = await this.renderSass(sassOptions);
+
+                // actually write it
+                await writeFile(fullOutputPath, result.css);
+
+                return {
+                    dependencies: [
+                        fullInputPath,
+                        ...result.stats.includedFiles
+                    ]
+                };
+            }
+        )
+    }
+}
+
+export default CompileScssMulti;
