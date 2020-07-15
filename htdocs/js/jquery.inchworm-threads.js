ba8a038d6d44ddac59f9fa91cb9e126697ded535
Nick Fagerlund <nick.fagerlund@gmail.com>
2020-07-15 16:04:08 -0700

    fix bugs

--- htdocs/js/jquery.inchworm-threads.js
+++ htdocs/js/jquery.inchworm-threads.js
@@ -1,8 +1,10 @@
 jQuery(function($){
     "use strict";
     $(document).on('click', '.js-comment-thread-focus-enable-trigger', function(e) {
+        e.preventDefault();
+        e.stopPropagation();
         var focusControl = $(e.target).closest('.js-comment-thread-focus');
-        var focusTarget = $(focusControl.data('target'));
+        var focusTarget = $('#' + focusControl.data('target'));
         var thread = focusControl.closest('.comment-thread');
         var toHide = thread.prevUntil(focusTarget);
         toHide.hide();
@@ -11,8 +13,10 @@ jQuery(function($){
     });
 
     $(document).on('click', '.js-comment-thread-focus-disable-trigger', function(e) {
+        e.preventDefault();
+        e.stopPropagation();
         var focusControl = $(e.target).closest('.js-comment-thread-focus');
-        var focusTarget = $(focusControl.data('target'));
+        var focusTarget = $('#' + focusControl.data('target'));
         var thread = focusControl.closest('.comment-thread');
         var toShow = thread.prevUntil(focusTarget);
         toShow.show();
