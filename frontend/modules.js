70ac75aba0ee0782e7e77d2a41c43df9e75c1953
Nick Fagerlund <nick.fagerlund@gmail.com>
2021-02-21 16:40:24 -0800

    More explanation in modules.js

--- frontend/modules.js
+++ frontend/modules.js
@@ -1,9 +1,14 @@
-// This is where we map modern components we bring in via npm modules to
-// wherever we want them to live in the static build directory. Use
-// broccoli-funnel objects to specify where things ought to go, then export them
-// all as an array.
+// This is where we map modern components from npm modules to wherever we want
+// them to live in the static build directory.
+
+// Use broccoli-funnel objects to specify where things ought to go, then export
+// all those funnels as an array.
+
+// Most of the time you'll probably want to use `resolve('module-name')` to
+// reach into an npm module's `dist` directory or something.
 
 import Funnel from 'broccoli-funnel';
+import { resolve } from 'path';
 
 let modernizr = new Funnel(
   './dist/modernizr', {
